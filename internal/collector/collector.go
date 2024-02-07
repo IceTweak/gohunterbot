0467c4798bb2f8a4b8e3293017d093226923174f
Roman Savoshchik <tweakice@gmail.com>
2024-02-08 00:23:47 +0600

    fix proxy data leak

--- internal/collector/collector.go
+++ internal/collector/collector.go
@@ -2,23 +2,11 @@ package collector
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/gocolly/colly/v2"
 )
-
-var DefaultProxyList = []string{
-	"socks5://kupfwikj:xm3642nd5169@38.154.227.167:5868",
-	"socks5://kupfwikj:xm3642nd5169@185.199.229.156:7492",
-	"socks5://kupfwikj:xm3642nd5169@185.199.228.220:7300",
-	"socks5://kupfwikj:xm3642nd5169@185.199.231.45:8382",
-	"socks5://kupfwikj:xm3642nd5169@188.74.210.207:6286",
-	"socks5://kupfwikj:xm3642nd5169@188.74.183.10:8279",
-	"socks5://kupfwikj:xm3642nd5169@188.74.210.21:6100",
-	"socks5://kupfwikj:xm3642nd5169@45.155.68.129:8133",
-	"socks5://kupfwikj:xm3642nd5169@154.95.36.199:6893",
-	"socks5://kupfwikj:xm3642nd5169@45.94.47.66:8110",
-}
 
 type CollectorOption func(c *colly.Collector) error
 
@@ -42,4 +30,21 @@ func NewCollector(options ...CollectorOption) (*colly.Collector, error) {
 		}
 	}
 	return c, nil
+}
+
+func GetDefaultProxyList() []string {
+	user := os.Getenv("PROXY_USER")
+	passwd := os.Getenv("PROXY_PASSWD")
+	return []string{
+		fmt.Sprintf("socks5://%s:%s@38.154.227.167:5868", user, passwd),
+		fmt.Sprintf("socks5://%s:%s@185.199.229.156:7492", user, passwd),
+		fmt.Sprintf("socks5://%s:%s@185.199.228.220:7300", user, passwd),
+		fmt.Sprintf("socks5://%s:%s@185.199.231.45:8382", user, passwd),
+		fmt.Sprintf("socks5://%s:%s@188.74.210.207:6286", user, passwd),
+		fmt.Sprintf("socks5://%s:%s@188.74.183.10:8279", user, passwd),
+		fmt.Sprintf("socks5://%s:%s@188.74.210.21:6100", user, passwd),
+		fmt.Sprintf("socks5://%s:%s@45.155.68.129:8133", user, passwd),
+		fmt.Sprintf("socks5://%s:%s@154.95.36.199:6893", user, passwd),
+		fmt.Sprintf("socks5://%s:%s@45.94.47.66:8110", user, passwd),
+	}
 }
